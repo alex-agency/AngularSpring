a7af786806eec3cea1816a4fba325b487b837ad1
Alex <@gmail.com>
2014-10-24 16:41:36 +0300

    Hello World

---
+++ src/main/java/spring/mvc/TestController.java
@@ -0,0 +1,13 @@
+package spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    public String test()
+    {
+        return "view";
+    }
+}
