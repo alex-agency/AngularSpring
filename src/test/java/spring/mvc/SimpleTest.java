43648bb3f5e32f63b2c89182c72d08b2ae6dc5f5
Alex <@gmail.com>
2014-10-24 17:44:49 +0300

    simple test

---
+++ src/test/java/spring/mvc/SimpleTest.java
@@ -0,0 +1,15 @@
+package spring.mvc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class SimpleTest {
+
+    @Test
+    public void test()
+    {
+
+    }
+}
