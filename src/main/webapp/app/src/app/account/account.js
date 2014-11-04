49606ff0157dbdb6b13b770c9a5ab1cdca327feb
Alex <@gmail.com>
2014-11-04 17:43:52 +0200

    added account and using session management with localStorage

---
+++ src/main/webapp/app/src/app/account/account.js
@@ -0,0 +1,50 @@
+angular.module('ngBoilerplate.account', ['ui.router'])
+    .config(function($stateProvider) {
+        $stateProvider.state('login', {
+            url:'/login',
+            views: {
+                'main': {
+                    templateUrl:'account/login.tpl.html',
+                    controller: 'LoginCtrl'
+                }
+            },
+            data : { pageTitle : "Login" }
+        })
+            .state('register', {
+                url:'/register',
+                views: {
+                    'main': {
+                        templateUrl:'account/register.tpl.html',
+                        controller: 'RegisterCtrl'
+                    }
+                },
+                data : { pageTitle : "Registration" }
+            }
+        );
+    })
+    .factory('sessionService', function() {
+        var session = {};
+        session.login = function(data) {
+            alert('user logged in with credentials ' + data.name + " and " + data.password);
+            localStorage.setItem("session", data);
+        };
+        session.logout = function() {
+            localStorage.removeItem("session");
+        };
+        session.isLoggedIn = function() {
+            return localStorage.getItem("session") !== null;
+        };
+        return session;
+    })
+    .controller("LoginCtrl", function($scope, sessionService, $state) {
+        $scope.login = function() {
+            sessionService.login($scope.account);
+            $state.go("home");
+        };
+    })
+    .controller("RegisterCtrl", function($scope, sessionService, $state) {
+        $scope.register = function() {
+            sessionService.login($scope.account);
+            $state.go("home");
+        };
+    });
